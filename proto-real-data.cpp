6454d10968d9c660fe3a9085487a07b0a87f1621
Zabirov Marat Ilsurovich <92zabirovmi@region.cbr.ru>
2021-09-14 11:21:13 +0300

    workable protobuf

--- proto-real-data.cpp
+++ proto-real-data.cpp
@@ -5,12 +5,20 @@
 // add proto data to value
 #include <iostream>
 
-#include "first.pb.h"
+//#include "first.pb.h"
+#include "block.pb.h"
 
 int main()
 {
     std::cout << "Test with proto data" << std::endl;
-
+    Block block{}; //= {"name", 1, 0};
+    block.set_id("name");
+    block.set_hash(1);
+    block.set_parent_hash(0);
+//    block.id = "name";
+//    block.hash = 1;
+//    block.parent_hash = 0;
+    std::cout << "Id: " << block.id() << " hash: " << block.hash() << " p hash: " << block.parent_hash() << std::endl;
 
 }
 
